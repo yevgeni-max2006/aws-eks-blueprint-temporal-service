8430ff38c4223c608e4962806f38675122a9bddf
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-07-04 22:39:35 +0300

    Add PostgreSQL DB instance and subnet group resources

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -1,1 +1,35 @@
 
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.name}-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = var.tags
+}
+
+resource "aws_db_instance" "this" {
+  identifier = var.name
+
+  engine         = "postgres"
+  engine_version = var.engine_version
+  instance_class = var.instance_class
+
+  allocated_storage = var.allocated_storage
+  storage_type      = "gp3"
+
+  db_name  = var.db_name
+  username = var.username
+  password = var.password
+
+  port = 5432
+
+  vpc_security_group_ids = var.vpc_security_group_ids
+  db_subnet_group_name    = aws_db_subnet_group.this.name
+
+  publicly_accessible = false
+  skip_final_snapshot = true
+
+  backup_retention_period = 7
+  deletion_protection     = false
+
+  tags = var.tags
+}
