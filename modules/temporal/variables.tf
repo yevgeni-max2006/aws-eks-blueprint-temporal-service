4743c235e133c7a2264515f50184bf18ecee06da
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-07-05 01:21:49 +0300

    Create variables.tf

---
+++ modules/temporal/variables.tf
@@ -0,0 +1,26 @@
+variable "db_host" {
+  type = string
+}
+
+variable "db_port" {
+  type    = number
+  default = 5432
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "namespace" {
+  type    = string
+  default = "temporal"
+}
