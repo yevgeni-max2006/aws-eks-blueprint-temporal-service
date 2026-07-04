f69a9cffdf4aebf6b7b1a70b5b6d553a768043cf
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-07-04 22:39:51 +0300

    Update variables.tf

--- modules/postgresql/variables.tf
+++ modules/postgresql/variables.tf
@@ -1,1 +1,30 @@
 
+variable "name" {}
+variable "db_name" {}
+variable "username" {}
+variable "password" {}
+
+variable "engine_version" {
+  default = "15.5"
+}
+
+variable "instance_class" {
+  default = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  default = 20
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "vpc_security_group_ids" {
+  type = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
